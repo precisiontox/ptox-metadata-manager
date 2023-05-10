c24e01266df6b8be2613dad7869e217d9c03d532
Terazus <batistadominique@hotmail.com>
2023-05-10 11:39:51 -0700

    missing docstring

--- ptmd/database/models/token_blocklist.py
+++ ptmd/database/models/token_blocklist.py
@@ -1,3 +1,6 @@
+""" Token block list model used to store expired JWT
+"""
+
 from ptmd.config import Base, db, jwt, session
 
 
@@ -9,7 +12,11 @@ class TokenBlocklist(Base):
     created_at = db.Column(db.DateTime, nullable=False)
 
 
-# Callback function to check if a JWT exists in the database blocklist
 @jwt.token_in_blocklist_loader
 def check_if_token_revoked(jwt_header: dict, jwt_payload: dict) -> bool:
+    """ Check if a JWT exists in the database blocklist
+
+    @param jwt_header: JWT header
+    @param jwt_payload: JWT payload
+    """
     return session.query(TokenBlocklist.id).filter_by(jti=jwt_payload["jti"]).scalar() is not None
