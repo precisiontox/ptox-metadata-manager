326f9bdbba704751d35ed234806972346d33df4d
Terazus <batistadominique@hotmail.com>
2023-05-11 10:08:00 -0700

    fixed more tests

--- tests/test_lib/test_email/test_validate_email.py
+++ tests/test_lib/test_email/test_validate_email.py
@@ -6,10 +6,11 @@ from ptmd.const import SITE_URL
 
 
 @patch("ptmd.lib.email.core.Credentials")
+@patch("ptmd.lib.email.core.get_config")
 class TestValidateEmail(TestCase):
 
     @patch('ptmd.lib.email.core.build')
-    def test_send_confirmation_mail(self, mock_build, mock_credentials):
+    def test_send_confirmation_mail(self, mock_build, mock_config, mock_credentials):
         mock_credentials.from_authorized_user_file.return_value = {'save_credentials_file': 'test'}
         message = send_confirmation_mail(username='test', email="test@test.com", token="test")
         self.assertIn("<h1> Hello, test</h1>", message)
