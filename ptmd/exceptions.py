be4918bd4f1a8c6315caea48eac07935680b78b8
Terazus <batistadominique@hotmail.com>
2023-11-29 02:59:12 -0800

    fixed docstrings

--- ptmd/exceptions.py
+++ ptmd/exceptions.py
@@ -1,9 +1,14 @@
+""" Custom exceptions for the ptmd package """
+
+
 class PasswordPolicyError(Exception):
     """ Exception raised when a password does not meet the password policy """
 
     def __init__(self) -> None:
-        self.message = "Password must between 8 and 20 characters long, contain at least one uppercase letter, one " \
-                       "lowercase letter, one number and one special character."
+        """ Constructor """
+        self.message: str = "Password must between 8 and 20 characters long, contain at least one uppercase letter, " \
+                            "one lowercase letter, one number and one special character."
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """ String representation of the exception """
         return self.message
