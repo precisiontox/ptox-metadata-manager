a7978ef9f3c710d7f3fe3ac1a6e214de852ad905
Terazus <batistadominique@hotmail.com>
2023-11-30 04:37:26 -0800

    fixed pep8

--- tests/test_api/test_queries/test_users.py
+++ tests/test_api/test_queries/test_users.py
@@ -311,7 +311,7 @@ class TestUserQueries(TestCase):
     def test_reset_password_error(self, mock_token,
                                   mock_get_current_user, mock_verify_jwt, mock_verify_in_request):
         mock_token.return_value.user_reset[0].set_password.side_effect = PasswordPolicyError
-        headers = {'Authorization': f'Bearer 123', **HEADERS}
+        headers = {'Authorization': 'Bearer 123', **HEADERS}
         with app.test_client() as client:
             response = client.post('/api/users/reset/456', data=dumps({"password": "None"}), headers=headers)
             self.assertEqual(response.json, {"msg": "Password must be between 8 and 20 characters long, contain at "
