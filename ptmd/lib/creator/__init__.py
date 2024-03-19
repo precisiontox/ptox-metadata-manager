7a572fd142681be91a7d7c8e14a92ed9123fc9e3
Terazus <batistadominique@hotmail.com>
2024-03-19 13:27:10 +0000

    updating docstrings

--- ptmd/lib/creator/__init__.py
+++ ptmd/lib/creator/__init__.py
@@ -1,6 +1,6 @@
 """ This module contains the Model class and functions that, given a list of fields, will generate the dataframe and
 ISA objects.
 
-@author: Terazus (D. Batista)
+:author: Terazus (D. Batista)
 """
 from .core import DataframeCreator
