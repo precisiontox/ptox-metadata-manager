48c37d2af6bc46286f4686d421b36d7481e7997f
Milo Thurston <knirirr@gmail.com>
2024-06-24 14:47:42 +0100

    Linting.

--- tests/test_api/test_queries/test_files/test_register.py
+++ tests/test_api/test_queries/test_files/test_register.py
@@ -150,7 +150,6 @@ class TestRegisterFile(TestCase):
             self.assertEqual(response.json, {'message': "File '123' does not exist."})
             self.assertEqual(response.status_code, 400)
 
-
     @patch('ptmd.api.queries.files.register.get_current_user')
     @patch('ptmd.api.queries.utils.verify_jwt_in_request')
     @patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
@@ -268,7 +267,6 @@ class TestRegisterFile(TestCase):
         mock_batch_updated.assert_called_with(batch='AA', filepath='filepath')
         self.assertEqual(response, 'filenameAA')
 
-
     @patch('ptmd.api.queries.files.register.session')
     @patch('ptmd.api.queries.files.register.GoogleDriveConnector', return_value=MockGoogleDriveError())
     @patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
@@ -281,8 +279,8 @@ class TestRegisterFile(TestCase):
     @patch('ptmd.api.queries.files.register.remove')
     @patch('ptmd.api.queries.files.register.get_shipped_file')
     def test_register_file_exception_upload(self, mocked_shipped_file, mock_rm, mock_data, mock_user, mock_organisation,
-                                        mock_get_user, mock_jwt_in_request, mock_file,
-                                        mock_verify_jwt, mock_gdrive, mock_session):
+                                            mock_get_user, mock_jwt_in_request, mock_file,
+                                            mock_verify_jwt, mock_gdrive, mock_session):
         mock_get_user().role = 'admin'
         mock_file.return_value.file_id = '123'
         mocked_shipped_file.return_value = None
